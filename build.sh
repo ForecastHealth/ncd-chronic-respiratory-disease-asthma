928997bc6a848e471d2ff27e4a68e52060d059f6
Rory Watts <rory@forecasthealth.org>
2024-07-22 13:55:28 +0800

    Updates to build

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 mkdir -p docs/models/ docs/scenarios/
 
 # Run the new shell scripts to generate model and scenario lists
-./generate_model_list.sh
-./generate_scenarios_list.sh
+./generate_models.sh
+./generate_scenarios.sh
 
 # Copy JSON files to docs directory
 cp ./list_of_countries.json ./docs/
