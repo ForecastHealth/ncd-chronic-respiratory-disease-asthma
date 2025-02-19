d3f1358c7c2fd0815fca5c5407886295fbe6b422
Rory Watts <rory@forecasthealth.org>
2025-02-19 12:38:08 +0800

    Working on new structure economic-analyses

---
+++ generate_economic_analyses.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+find ./economic-analysis -name '*.json' -print0 | while IFS= read -r -d '' file; do
+    title=$(jq -r '.name // empty' "$file")
+    
+    # If title is missing, use the filename
+    [ -z "$title" ] && title=$(basename "$file")
+    
+    # Get just the filename without the path
+    filename=$(basename "$file")
+    
+    # Output the JSON object for this report
+    echo "{\"name\": \"$title\", \"filename\": \"$filename\"}"
+done | jq -s 'sort_by(.title) | { economic_analyses: . }' > list_of_economic_analyses.json
+
+echo "Generated list_of_economic_analyses.json"
